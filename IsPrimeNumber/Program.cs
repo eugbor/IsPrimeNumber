9b015af6f38d8aee9a1cf3476e87eba71916a61d
eugbor <eugeneborodin@mail.ru>
2017-01-04 01:41:50 +0300

    init

---
+++ IsPrimeNumber/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+// Определите, является ли натуральное число простым.
+//
+namespace IsPrimeNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(IsPrime(n));
+            Console.ReadLine();
+        }
+
+        static bool IsPrime(int n)
+        {
+            for (int i = 2; i <= n/i; i++)
+            {
+                if (n%i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
